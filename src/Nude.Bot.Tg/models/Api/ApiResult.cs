6d23e25802c92483bad6ed01b6540c25ce7673c6
Igordeveloperr <g.bikmetow@gmail.com>
2023-04-13 22:07:54 +0300

    upd: created new DTO

---
+++ src/Nude.Bot.Tg/models/Api/ApiResult.cs
@@ -0,0 +1,17 @@
+﻿using Nude.API.Contracts.Errors.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nude.Bot.Tg.models.Api
+{
+    public class ApiResult<T>
+    {
+        public T? Result { get; set; }
+        public bool IsSuccess => ErrorResponse == null;
+        public string? Message { get; set; }
+        public ErrorResponse? ErrorResponse { get; set; }
+    }
+}
